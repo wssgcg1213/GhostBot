ff375c791fb01c87cc050112a41980fd9d3e6eaf
Luna Shu <luna_yj@qq.com>
2017-05-16 20:08:07 +0800

    修改rss 为 API (#2)
    * Update README.md
    
    * Update ghostbot.js
    
    * Update ghostbot.min.js
    
    * Update README.md
    
    * Update README.md
    
    * Update ghostbot.min.js
    
    * Update ghostbot.js
    
    * Update README.md

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
 GhostBot
 ---
-####A Common Search Engine based On RSS
-####基于RSS的通用搜索引擎
+#### A Common Search Engine based On Ghost API
+#### 基于Ghost API的通用搜索引擎
 ---
 The plugin is written by [Zeroling](http://www.zeroling.com).
+Updated by [Luna Shu](https://luna.fancylog.net).
 
-The plugin allows front-end engineer to add Search Engine to your platform based on rss. Similar but unlike GhostHunter ,there is no need of jQuery or lunr.js.
+The plugin allows front-end engineer to add Search Engine to your platform based on Ghost API. Similar but unlike GhostHunter ,there is no need of jQuery or lunr.js.
 
-这个插件不需要任何依赖, 加载方式类似于博客系统Ghost的开源插件[GhostHunter](https://github.com/i11ume/ghostHunter/), 但是不需要 jQuery 和 lunr.js 的支持.
+这个插件不需要任何依赖, 加载方式类似于博客系统Ghost的开源插件[GhostHunter](https://github.com/i11ume/ghostHunter/), 但是不需要 jQuery 和 lunr.js 的支持.
 
-通过Ajax读取并解析 rss, 在 Chrome 和 Safari 环境下支持良好, 其他环境未测试.
+通过Ghost API 获取全站文章, 在 Chrome 和 Safari 环境下支持良好, 其他环境未测试.
 
 If you find it useful, it's what I'd like to see.
 
-If you cannot run it properly, or you have any good suggestions, cantact me and maybe I could make it better: <wssgcg1213@qq.com>.
+If you cannot run it properly, or you have any good suggestions, cantact me and maybe I could make it better: <luna_shu@fancylog.net>.
 
 Usage
 ---
@@ -57,4 +58,3 @@ Options
 - `optional` result_template: result template, we will use each result to replace {{link}}, {{title}}, {{content}}.   结果模板, {{link}}, {{title}}, {{content}}会被每一条搜索结果替换.
 `default` : `"<a href='{{link}}' class='searchResult'>{{title}}</a>"`
 
-- `optional` rss: the rss address. RSS 的地址. `default`: `/rss`
--- ghostbot.js
+++ ghostbot.js
@@ -1,151 +1,144 @@
 /**
- * GhostBot: A Simple Common Search Engine based On RSS
- * author: ZeroLing
- * Git: https://github.com/wssgcg1213/ghostbot
- * http://www.zeroling.com/
+ * GhostBot: A Simple Common Search Engine based on Ghost API
+ * author: Luna Shu
+ * Git: https://github.com/LunaYJ/GhostBot.git
+ * coding: https://git.coding.net/lunayj/GhostBot.git
+ * https://luna.fancylog.net/
+ *
+ * Forked from https://github.com/wssgcg1213/ghostbot
+ * Original Author: ZeroLing
+ * Original Author Blog: http://www.zeroling.com/
+ *
  */
-
 function Ajax() {
-	"use strict";
-	var aja = {};
-	aja.tarUrl = '';
-	aja.postString = '';
-	aja.createAjax = function() {
-		var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-		return xmlhttp;
-	}
-	aja.xhr = aja.createAjax();
-	aja.processHandler = function() {
-		if (aja.xhr.readyState == 4) 
-			if (aja.xhr.status == 200) 
-				aja.resultHandler(aja.xhr.responseText);
-	}
-
-	aja.get = function(tarUrl, callbackHandler) {
-		aja.tarUrl = tarUrl;
-		aja.resultHandler = callbackHandler;
-		aja.xhr.onreadystatechange = aja.processHandler;
-		aja.xhr.open('get', aja.tarUrl, true);
-		aja.xhr.send();
-
-	}
-	return aja;
+  "use strict";
+  var aja = {};
+  aja.tarUrl = '';
+  aja.postString = '';
+  aja.createAjax = function() {
+    var xmlhttp = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    return xmlhttp;
+  }
+  aja.xhr = aja.createAjax();
+  aja.processHandler = function() {
+    if (aja.xhr.readyState == 4)
+      if (aja.xhr.status == 200)
+        aja.resultHandler(aja.xhr.responseText);
+  }
+  aja.get = function(tarUrl, callbackHandler) {
+    aja.tarUrl = tarUrl;
+    aja.resultHandler = callbackHandler;
+    aja.xhr.onreadystatechange = aja.processHandler;
+    aja.xhr.open('get', aja.tarUrl, true);
+    aja.xhr.send();
+  }
+  return aja;
 }
-
 var GhostBot = function(options) {
-	this.defaults = {
-		result_template: "<a href='{{link}}' class='searchResult'>{{title}}</a>",
-		info_template: "<h4>Find{{amount}}Articles.</h4>",
-		rss: "/rss",
-	}
-	var opts = this.extend({}, this.defaults, options);
-	if (opts.inputbox) this.init(opts);
+  this.defaults = {
+    result_template: "<a href='{{link}}' class='searchResult'>{{title}}</a>",
+    info_template: "<h4>Find{{amount}}Articles.</h4>",
+  }
+  var opts = this.extend({}, this.defaults, options);
+  if (opts.inputbox) this.init(opts);
 }
 GhostBot.prototype.extend = function() {
-	var _arg = Array.prototype.slice.call(arguments);
-	for (var i = _arg.length - 1; i > 0; i--) {
-		var former = _arg[i - 1],
-			latter = _arg[i];
-		for (j in latter) former[j] = latter[j];
-	}
-	return _arg[0];
+  var _arg = Array.prototype.slice.call(arguments);
+  for (var i = _arg.length - 1; i > 0; i--) {
+    var former = _arg[i - 1],
+      latter = _arg[i];
+    for (j in latter) former[j] = latter[j];
+  }
+  return _arg[0];
 }
-
 GhostBot.prototype.init = function(opts) {
-	var that = this;
-	this.result_template = opts.result_template;
-	this.info_template = opts.info_template;
-	this.target = opts.target;
-	this.rss = opts.rss;
-	this.inputbox = opts.inputbox;
-	this.blogData = [];
-	this.ajax = new Ajax();
-	this.loadRss();
+  var that = this;
+  this.result_template = opts.result_template;
+  this.info_template = opts.info_template;
+  this.target = opts.target;
+  this.inputbox = opts.inputbox;
+  this.blogData = [];
+  this.ajax = new Ajax();
+  this.loadAPI();
 }
-
-GhostBot.prototype.loadRss = function() {
-	if (this.inited) return false;
-	var index = this.index,
-		rssUrl = this.rss,
-		blogData = this.blogData,
-		that = this;
-	this.ajax.get(rssUrl, function(data) {
-		that.parseXML(data, function(xmlDoc) {
-			var items = xmlDoc.getElementsByTagName('item');
-			items = Array.prototype.slice.call(items);
-			items = items.map(function(i) {
-				if (typeof i == 'function') return;
-				return i;
-			});
-			that.items = items;
-			that.inited = true;
-			that.listen();
-		});
-	})
+GhostBot.prototype.loadAPI = function(){
+  if (this.inited) return false;
+  var index = this.index;
+  var that = this;
+  var obj		= {limit: "all",  include: "tags"};
+  var maxLength = 120;
+  var blogData = [];
+  this.ajax.get(ghost.url.api('posts',obj), function(data) {
+    var searchData = JSON.parse(data).posts;
+    searchData.forEach(function(arrayItem){
+      var tag_arr = arrayItem.tags.map(function(v) {
+        return v.name;
+      })
+      if(arrayItem.meta_description == null) { arrayItem.meta_description = '' };
+      var category = tag_arr.join(", ");
+      if (category.length < 1){
+        category = "undefined";
+      }
+      var parsedData 	= {
+        id 			: String(arrayItem.id),
+        title 		: String(arrayItem.title),
+        slug : String(arrayItem.slug),
+        url: String(arrayItem.url),
+        markdown 	: String(arrayItem.markdown),
+        pubDate 	: String(arrayItem.created_at.split('T')[0]),
+        tag 		: category,
+        link 		: String(arrayItem.url),
+        description	: String(arrayItem.markdown.substr(0, maxLength)),
+        content : arrayItem.markdown
+      };
+      blogData.push(parsedData);
+    });
+    that.items = blogData;
+    that.inited = true;
+    that.listen();
+  })
 }
-
-GhostBot.prototype.parseXML = function(xmlString, callback) {
-	var xmlDoc = null;
-	if (!window.DOMParser && window.ActiveXObject) {    //支持IE浏览器 
-		var xmlDomVersions = ['MSXML.2.DOMDocument.6.0', 'MSXML.2.DOMDocument.3.0', 'Microsoft.XMLDOM'];
-		for (var i = 0; i < xmlDomVersions.length; i++) {
-			try {
-				xmlDoc = new ActiveXObject(xmlDomVersions[i]);
-				xmlDoc.async = false;
-				xmlDoc.loadXML(xmlString);
-				break;
-			} catch (e) {}
-		}
-	} else if (window.DOMParser && document.implementation && document.implementation.createDocument) {
-		try {
-			domParser = new DOMParser();
-			xmlDoc = domParser.parseFromString(xmlString, 'text/xml');
-		} catch (e) {}
-	} else {
-		return null;
-	}
-	callback && callback(xmlDoc);
-	return xmlDoc;
-}
-
 GhostBot.prototype.listen = function() {
-	var that = this;
-	this.inputbox.onkeyup = function() {
-		var ele = that.inputbox;
-		if (!ele.value) return that.target.innerHTML = '';
-		var _r = that.search(ele.value); //[{}, {}]
-		var info_parsed = that.format(that.info_template, {
-			amount: _r.length
-		});
-		var _HTML = info_parsed;
-		for (i in _r) _HTML += that.format(that.result_template, _r[i]);
-		that.target.innerHTML = _HTML;
-	}
+  var that = this;
+  this.inputbox.onkeyup = function() {
+    var ele = that.inputbox;
+    if (!ele.value) return that.target.innerHTML = '';
+    var _r = that.search(ele.value); //[{}, {}]
+    var info_parsed = that.format(that.info_template, {
+      amount: _r.length
+    });
+    var _HTML = info_parsed;
+    for (i in _r) _HTML += that.format(that.result_template, _r[i]);
+    that.target.innerHTML = _HTML;
+  }
 }
-
 GhostBot.prototype.format = function(text, obj) {
-	return text.replace(/{{([^{}]*)}}/g, function(a, b) {
-		var r = obj[b];
-		return typeof r === "string" || typeof r === "number" ? r : a
-	})
+  return text.replace(/{{([^{}]*)}}/g, function(a, b) {
+    var r = obj[b];
+    return typeof r === "string" || typeof r === "number" ? r : a
+  })
 }
-
 GhostBot.prototype.search = function(kw) {
-	var that = this;
-	var _result = [];
-	var _reg = new RegExp(kw.toLowerCase());
-	this.items.forEach(function(i) {
-		var content = i.getElementsByTagName('description')[0].firstChild.data.replace(/<!((\[)?\[)|(\]\]>)/g, '').replace(/<[^>]*>/g, '');
-		var title = i.getElementsByTagName('title')[0].firstChild.data.replace(/<!((\[)?CDATA\[)|(\]\]>)/g, '');
-		if (_reg.test(title.toLowerCase() + content.toLowerCase())) {
-			var link = i.getElementsByTagName('link')[0].firstChild.data;
-			_result.push({
-				keyword: kw,
-				title: title,
-				link: link,
-				content: content
-			});
-		}
-	});
-	return _result;
+  var that = this;
+  var _result = [];
+  var _reg = new RegExp(kw.toLowerCase());
+  this.items.forEach(function(i) {
+    var content = i.markdown;
+    var title = i.title;
+    var pubDate = i.pubDate,
+      url = i.url,
+      slug = i.slug,
+      description	= i.description;
+    if(_reg.test(title.toLowerCase() + content.toLowerCase() + url.toLowerCase())){
+      var link = i.link;
+      _result.push({
+        title: title,
+        link: link,
+        content: content,
+        pubData : i.pubDate,
+        description : i.description
+      })
+    }
+  });
+  return _result;
 }
